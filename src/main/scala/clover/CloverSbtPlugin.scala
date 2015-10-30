0be8417938cb32cc98fc740d0cb0d01e9a84eae6
Shanbin Wang <shanbin.wang@gmail.com>
2015-10-30 14:15:45 -0700

    Enable JVM forking in test to flush clover output

--- src/main/scala/clover/CloverSbtPlugin.scala
+++ src/main/scala/clover/CloverSbtPlugin.scala
@@ -8,9 +8,9 @@ import sbt._
 
 object Imports {
   object CloverKeys {
-    lazy val setup = taskKey[Unit]("instrument sources")
-    lazy val test = taskKey[Unit]("compile and test with clover instrumented sources")
-    lazy val clover = taskKey[Unit]("instrument, compile, test, and generate XML and HTML reports")
+    lazy val setup = taskKey[Unit]("instrument sources with clover")
+    lazy val test = taskKey[Unit]("alias for test:test, overrides javaSource and fork")
+    lazy val clover = taskKey[Unit]("generate clover XML and HTML reports, depends on setup and test")
     lazy val cloverLicensePath = settingKey[String]("clover license location")
     lazy val cloverVersion = settingKey[String]("clover version number, e.g. 4.0.6")
   }
@@ -36,39 +36,39 @@ object CloverSbtPlugin extends AutoPlugin {
       cloverLicensePath := (baseDirectory.value / "clover.license").toString,
       cloverVersion := "4.0.6",
       setup <<= setupDef,
-      test <<= testDef dependsOn setup,
+      test <<= (test in Test) dependsOn setup,
       clover <<= reportDef dependsOn test,
       javaSource in Compile <<= cloverSource,
-      javaSource in Test <<= cloverSourceT
+      javaSource in Test <<= cloverSourceT,
+      fork in Test := true
     )
   ) ++ commonSettings
 
   def setupDef = Def.task[Unit] {
     System.setProperty("clover.license.path", cloverLicensePath.value)
 
-    streams.value.log.info("Got clover license from " + System.getProperty("clover.license.path"))
+    streams.value.log.debug("clover.license.path = " + System.getProperty("clover.license.path"))
 
     val source = (sourceDirectory in Compile).value / "java"
     val sourceT = (sourceDirectory in Test).value / "java"
     val cliArgs1 = Array("-i", cloverDb.value.toString, "-d", cloverSource.value.toString, "-s", source.toString)
     val cliArgs2 = Array("-i", cloverDb.value.toString, "-d", cloverSourceT.value.toString, "-s", sourceT.toString)
 
-    // instrument sources
+    // instrument main and test sources
     CloverInstr.mainImpl(cliArgs1)
     CloverInstr.mainImpl(cliArgs2)
   }
 
-  def testDef = Def.task {
-    (test in Test).value
-  }
-
   def reportDef = Def.task {
     streams.value.log.info("Generating Clover XML and HTML reports")
+
     val cloverReports = cloverDir.value / "reports"
     val xmlArgs = Array("-i", cloverDb.value.toString, "-o", (cloverReports / "clover.xml").toString)
     val htmlArgs = Array("-i", cloverDb.value.toString, "-o", (cloverReports / "html").toString)
+
     XMLReporter.runReport(xmlArgs)
     HtmlReporter.runReport(htmlArgs)
+
     () // return Unit
   }
 
